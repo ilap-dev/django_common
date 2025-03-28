dff4d50cec756a54005009f08f6f01f4774bfff4
ilap42 <ilap435@gmail.com>
2025-03-28 09:42:11 -0600

    Delete TestView

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import TestView
+
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('test/', TestView.as_view()),
+    #path('test/', TestView.as_view()),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/core/views.py
+++
@@ -1,8 +0,0 @@
-
-##Forma mas basica de crear un API
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class TestView(APIView):
-    def get(self,request,*args, **kwargs):
-        return Response("Hola Mundo")
