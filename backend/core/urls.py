218d3fe9231a77b9998097a5209aa88f5ab3df71
ilap42 <ilap435@gmail.com>
2025-03-28 09:50:27 -0600

    Delete TestView page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    ###path('test/', TestView.as_view()),
+    ####path('test/', TestView.as_view()),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
